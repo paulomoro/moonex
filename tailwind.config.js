cb11f4bf5817adb399fabd6b853acf01a9147925
paul <paulomoro20@gmail.com>
2024-12-11 10:31:21 +0100

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import animate from 'tailwindcss-animate';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
@@ -99,5 +101,5 @@ theme: {
     }
   }
 },
-plugins: [require("tailwindcss-animate")]
+plugins: [animate]
 }
